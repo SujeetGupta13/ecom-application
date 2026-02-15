428635f1a5288fc63faf15066e8554935cd9389d
SujeetGupta13 <sujeetgupta13hr@gmail.com>
2026-02-15 11:50:27 +0530

    E com app till service layer

---
+++ src/main/java/com/app/ecom/UserController.java
@@ -0,0 +1,51 @@
+package com.app.ecom;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor //This will generate constructor only for final fields.
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    //ResponseEntity to have better control over response code.
+    @GetMapping("/{id}")
+    //@RequestMapping(value = "api/users/{id}", method = RequestMethod.GET)
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+//        User user = userService.fetchUser(id);
+//        if (user == null) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        return new ResponseEntity<>(user, HttpStatus.OK);
+        return userService.fetchUser(id)
+                //.map(user -> ResponseEntity.ok(user))
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok(userService.fetchAllUsers());
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody User user){
+       userService.addUser(user);
+       return new ResponseEntity<>("User has been added successfully", HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User updatedUser){
+        boolean updated = userService.updateUser(id, updatedUser);
+        if (updated) {
+            return ResponseEntity.ok("User has been updated successfully");
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
